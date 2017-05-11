a1bd94e28ee190b7c62eebb90fe510dc3aba736a
John Fearnside <jfearnside@avvo.com>
2017-05-10 21:01:16 -0700

    Update deps restrictions

--- mix.exs
+++ mix.exs
@@ -27,8 +27,8 @@ defmodule Avrolixr.Mixfile do
       {:erlavro, git: "https://github.com/avvo/erlavro", ref: "fb7c7f0"},
       {:poison, "~> 2.0"},
       # NON-PRODUCTION DEPS
-      {:dialyxir, "~> 0.4", only: [:dev]},
-      {:ex_doc, ">= 0.0.0", only: [:dev]}
+      {:dialyxir, "~> 0.5", only: [:dev]},
+      {:ex_doc, "~> 0.15", only: [:dev]}
     ]
   end
 
@@ -41,9 +41,9 @@ defmodule Avrolixr.Mixfile do
   defp package do
     [
       name: :avrolixr,
-      files: ["lib", "mix.exs", "CHANGELOG.md", "README.md", "LICENSE.txt"],
+      files: ~w[lib mix.exs CHANGELOG.md README.md LICENSE.txt],
       maintainers: ["Donald Plummer", "John Fearnside"],
-      licenses: ["MIT"],
+      licenses: ~w[MIT],
       links: %{
         "GitHub" => "https://github.com/avvo/avrolixr",
         "Docs" => "https://hexdocs.pm/avrolixr"
